fcfe5eab481787d43c8a51e43c42a130f068cb95
secadavida <142696326+secadavida@users.noreply.github.com>
2023-08-21 23:39:23 +0000

    taller 2 python - ejercicio 2

---
+++ main.py
@@ -0,0 +1,47 @@
+class Asiento:
+	def __init__(self, color, precio, registro):
+		self.color = color
+		self.precio = precio
+		self.registro = registro
+
+	def cambiarColor(self, color):
+		if color in ["rojo", "verde", "amarillo", "negro", "blanco"]:
+			self.color = color
+
+class Motor:
+	def __init__(self, numeroCilindros, tipo, registro):
+		self.numeroCilindros = numeroCilindros
+		self.tipo = tipo
+		self.registro = registro
+
+	def cambiarRegistro(self, registro):
+		self.registro = registro
+
+	def asignarTipo(self, tipo):
+		self.tipo = tipo
+
+class Auto:
+	cantidadCreados = 0
+
+	def __init__(self, modelo, precio,	asientos, marca, motor, registro):
+		self.modelo = modelo
+		self.precio = precio
+		self.asientos = asientos
+		self.marca = marca
+		self.motor = motor
+		self.registro = registro
+
+	def cantidadAsientos(self):
+		numeroAsientos = 0
+		for a in self.asientos:
+			if type(a) == Asiento:
+				numeroAsientos += 1
+
+		return numeroAsientos
+
+	def verificarIntegridad(self):
+		for a in self.asientos:
+			if a.registro != self.registro != self.motor.registro:
+				return "Las piezas no son originales"
+
+		return "Auto original"
